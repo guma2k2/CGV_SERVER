4e7d767c6e9c3dc64adf871da2f6b62347371dff
Thuan ngo <thuanngo3072002@gmail.com>
2024-09-05 21:55:08 +0700

    deploy

--- src/test/java/com/movie/backend/BackendCinemaApplicationTests.java
+++ src/test/java/com/movie/backend/BackendCinemaApplicationTests.java
@@ -1,12 +1,12 @@
-package com.movie.backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendCinemaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-}
+//package com.movie.backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BackendCinemaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//}
