79934a0e31da8ac5456e737d094950d8a27fcb76
Thuan ngo <thuanngo3072002@gmail.com>
2025-04-12 20:29:15 +0700

    modify features

--- src/main/java/com/movie/backend/entity/User.java
+++ src/main/java/com/movie/backend/entity/User.java
@@ -37,6 +37,7 @@ public class User implements UserDetails {
     private boolean status;
 
     private String verificationCode;
+
     private String forgotPassword ;
 
     @Enumerated(EnumType.STRING)
